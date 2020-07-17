239f6c932851c15c65db383e2e2312301cc063cb
ksk001100 <hm.pudding0715@gmail.com>
2020-07-17 17:12:56 +0900

    first commit

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ fn main() {
     let app = App::new(env!("CARGO_PKG_NAME"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .version(env!("CARGO_PKG_VERSION"))
+        .description(env!("CARGO_PKG_DESCRIPTION"))
         .usage(format!("{} [command] [args]", env!("CARGO_PKG_NAME")))
         .command(hello::command());
 
